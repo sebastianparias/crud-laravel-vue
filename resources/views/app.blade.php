7f2814a76c95c2fea2d07b69c155afadcc810bb9
sebastianparias <70963773+sebastianparias@users.noreply.github.com>
2022-07-18 06:37:08 -0500

    1807

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,6 @@
     <title>Document</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    {{-- <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet"> --}}
     <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet" />
 </head>
 
